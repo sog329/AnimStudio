2558e26416a257ab744dda0ce19574cf2bb88910
sog329 <songxg0329@163.com>
2020-01-11 18:33:25 +0800

    modify ui for CardView

--- app/src/main/java/com/sunshine/studio/base/ItemContainer.java
+++ app/src/main/java/com/sunshine/studio/base/ItemContainer.java
@@ -3,12 +3,12 @@ package com.sunshine.studio.base;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 
 /** Created by songxiaoguang on 2018/1/4. */
-public class ItemContainer extends FrameLayout {
+public class ItemContainer extends CardView {
   public ItemContainer(@NonNull Context context) {
     super(context);
   }
